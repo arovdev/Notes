899d54d4efcdeac22559656cdc001a9fafd6b0e9
arovdev <arthur.reshetniak@gmail.com>
2024-05-03 14:17:18 -0300

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Arthur Reshetnyak on 2024-05-03.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
